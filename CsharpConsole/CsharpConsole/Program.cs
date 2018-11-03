e229d7b7cfc9ee00de054b5509770ff55cae02f2
Sivanandham <siva24anand@gmail.com>
2018-11-03 17:19:09 +0530

    Console app initial commit
    Console app initial commit

---
+++ CsharpConsole/CsharpConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "At last went to github";
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
