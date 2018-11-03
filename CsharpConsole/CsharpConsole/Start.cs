72f316a0b0157ebc887b835698f1edaee67a4ca1
Sivanandham <siva24anand@gmail.com>
2018-11-03 19:57:37 +0530

    asynawait added

--- CsharpConsole/CsharpConsole/Start.cs
+++ CsharpConsole/CsharpConsole/Start.cs
@@ -9,14 +9,19 @@ namespace CsharpConsole
 {
     public class Start
     {
-        bool _delegate = true;
-
+        bool _delegate = false;
+        bool _asynAwait = true;
         public void Initialize()
         {
             if (_delegate)
             {
                 var delg = new DelegatesSample();
                 delg.Execute();
+            }
+            if (_asynAwait)
+            {
+                var asaw = new AsynAwait();
+                asaw.Execute();
             }
         }
     }
---
+++ CsharpConsole/CsharpConsole/Topics/AsynAwait.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsharpConsole.Topics
+{
+    public class AsynAwait
+    {
+        public void Execute()
+        {
+            CallMethod();
+        }
+        static async void CallMethod()
+        {
+            string filePath = "fakepath";
+            Task<int> task = ReadFile(filePath);
+
+            Console.WriteLine(" Other Work 1");
+            Console.WriteLine(" Other Work 2");
+            Console.WriteLine(" Other Work 3");
+
+            int length = await task;
+            Console.WriteLine(" Total length: " + length);
+
+            Console.WriteLine(" After work 1");
+            Console.WriteLine(" After work 2");  
+
+        }
+        public static async Task<int> ReadFile(string str)
+        {
+            Console.WriteLine(" Other Work Readfile 1");
+            await Task.Run(() => 
+                {
+                    Thread.Sleep(5000);
+                });
+            Console.WriteLine(" Other Work Readfile 2");
+            Console.WriteLine(" Other Work Readfile 3");
+            return 5;
+        }
+    }
+}
